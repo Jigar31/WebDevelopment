46e7a4ba69c4a5af683991f0c7b32455ab808f94
Jigar31 <jigarpunjani@gmail.com>
2016-12-23 18:46:24 +0530

    Adding Project1 which contains JavaScript

---
+++ project1/project1.js
@@ -0,0 +1,6 @@
+/*jslint browser: true, devel: true*/ //for solving undeclared 'alert' error
+/*global window*/ //for eliminating undeclared 'window' error
+var a = 7;
+var b = 8;
+
+window.alert(a + " + " + b + " is " + (a + b));
