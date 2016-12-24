48a1d8ba11094911f67d80aabe9cb3a415850e59
Jigar31 <jigarpunjani@gmail.com>
2016-12-24 21:43:20 +0530

    removing old files and adding Exercise 5 - Functions

---
+++ project1/functions.js
@@ -0,0 +1,8 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+function saySomething() {
+    "use strict";
+    window.alert("This is a function");
+}
+
+saySomething();
--- project1/project1.js
+++
@@ -1,6 +0,0 @@
-/*jslint browser: true, devel: true*/ //for solving undeclared 'alert' error
-/*global window*/ //for eliminating undeclared 'window' error
-var a = 7;
-var b = 8;
-
-window.alert(a + " + " + b + " is " + (a + b));
