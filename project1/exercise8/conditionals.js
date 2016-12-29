dbd44804710281afb0ad945c4a6f31562c0a6c01
Jigar31 <jigarpunjani@gmail.com>
2016-12-29 19:44:14 +0530

    Exercise 8 conditions (if...else)

---
+++ project1/exercise8/conditionals.js
@@ -0,0 +1,19 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+
+var food = "Apple";
+
+if (food === "Apple") {
+    alert("We ate an Apple");
+} else {
+    alert("We ate something else");
+}
+
+// if() {
+//
+// }
+// else {
+//
+// }
+// will provide error "only one space expected between '}' and 'else' " from jslint so use above form.
+// === is identity operator, used to test that value and type are equal
