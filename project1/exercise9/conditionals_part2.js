98a659caae6c2bd30481bc62fb1c566dbdb329ca
Jigar31 <jigarpunjani@gmail.com>
2016-12-29 20:09:30 +0530

    Exercise 9 conditions part2 usage of else if()

---
+++ project1/exercise9/conditionals_part2.js
@@ -0,0 +1,20 @@
+/*jslint browser: true devel: true white: true*/
+/*global window*/
+
+var food = "Fast Food";
+
+if (food === "Apple") {
+    alert ("We ate an Apple");
+}
+else if (food === "Salad") {
+    alert ("We ate the Salad");
+}
+else if (food === "Fast Food") {
+    alert ("We ate Fast Food");
+}
+else {
+    alert("We ate something else");
+}
+
+// use jslint white: true to ignore Expected one space between '}' and 'else' error
+// but it's usage is strictly not recommended
