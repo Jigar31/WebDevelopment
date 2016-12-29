3705dbe605272699e29b71cb397098faf767b3b8
Jigar31 <jigarpunjani@gmail.com>
2016-12-29 21:04:47 +0530

    Exercise 11 loops part2 usage of for loop

---
+++ project1/exercise11/loops_part2.js
@@ -0,0 +1,10 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+
+var i;
+
+for (i = 1; i < 5; i += 1) {
+    document.write("This statement is printed <br />");
+}
+
+//using loops outside a function is not recommended by jslint
