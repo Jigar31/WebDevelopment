153e7e6fe8c9e4f6f843999da24153cc8bf03786
Jigar31 <jigarpunjani@gmail.com>
2016-12-30 19:45:18 +0530

    Exercise 19 String object and pre-built string functions

---
+++ project1/exercise19/stringObject.js
@@ -0,0 +1,32 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+
+function print(a) {
+    "use strict";
+    document.write(a);
+    document.write("<br />");
+}
+
+var hello = "Hello! How are you doing?";
+print(hello);
+
+var upHello = hello.toUpperCase();
+print(upHello);
+
+var lowHello = hello.toLowerCase();
+print(lowHello);
+
+var lengthHello = hello.length;
+print(lengthHello);
+
+var charHello = hello.charAt(9);
+print(charHello);
+
+var replaceDoing = hello.replace('doing', 'today');
+print(replaceDoing);
+
+var boldHello = hello.bold();
+print(boldHello);
+
+var italicsHello = hello.italics();
+print(italicsHello);
