056fd5ddb27a114d38197a4d4ddc8b236ede9338
Jigar31 <jigarpunjani@gmail.com>
2016-12-31 15:19:58 +0530

    Exercise 22 DOM model and usage

---
+++ project1/exercise22/dom.js
@@ -0,0 +1,10 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+
+function changeStyle() {
+    'use strict';
+    document.getElementById("para1").style.color = "blue";
+    document.getElementById("para1").style.backgroundColor = "yellow";
+    document.getElementById("para1").style.fontStyle = "italic";
+    document.getElementById("para1").style.paddingLeft = "50px";
+}
