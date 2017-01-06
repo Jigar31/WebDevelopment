c9d6c5d287860a13fc0e6ab5dfa7a57bed3b628b
Jigar31 <jigarpunjani@gmail.com>
2017-01-06 12:46:48 +0530

    Exercise 30 mouseover and mouseout event for changing images

---
+++ project1/exercise30/mouseoverImage.js
@@ -0,0 +1,12 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+
+function changeImage() {
+    'use strict';
+    document.getElementById("image").src = "ice-cream-sundae.jpg";
+}
+
+function backToOriginalImage() {
+    'use strict';
+    document.getElementById("image").src = "Ice-cream2.jpg";
+}
