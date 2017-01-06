557fb62d6c53e3498c229d76321fd871e98a49c1
Jigar31 <jigarpunjani@gmail.com>
2017-01-06 12:29:54 +0530

    Exercise 29 mouseover and mouseout event

---
+++ project1/exercise29/mouseoverEvent.js
@@ -0,0 +1,12 @@
+/*jslint browser: true devel: true*/
+/*global window*/
+
+function changeBackground() {
+    'use strict';
+    document.getElementById("para").style.backgroundColor = "blue";
+}
+
+function backToNormal() {
+    'use strict';
+    document.getElementById("para").style.backgroundColor = "";
+}
